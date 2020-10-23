df6b94c10d32cbae9ddcbfa6c1464b7b0533c251
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-23 20:27:16 +0200

    Delete unused files, added 3 webservers

--- lib/ubuntu_boot.bash
+++ lib/ubuntu_boot.bash
@@ -23,6 +23,11 @@ apt-get update
 apt-get -y install puppet-agent
 echo "$(/opt/puppetlabs/bin/facter networking.ip) $(hostname).node.consul $(hostname)" >> /etc/hosts
 echo "manager_ip_address manager.node.consul manager" >> /etc/hosts
+
 /opt/puppetlabs/bin/puppet config set server manager.node.consul --section main
 /opt/puppetlabs/bin/puppet config set runinterval 300 --section main
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+/opt/puppetlabs/bin/puppet agent -t # request certificate
+/opt/puppetlabs/bin/puppet agent -t # configure manager
+
+
